b482bf221bfc182ff9755c97eb78e77b682ba213
Alysson Oliveira <3oalysson@gmail.com>
2023-09-11 18:38:26 -0300

    Inicial

---
+++ src/main/java/com/dunkelhai/javaspringcache/JavaSpringCacheApplication.java
@@ -0,0 +1,13 @@
+package com.dunkelhai.javaspringcache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringCacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringCacheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dunkelhai/javaspringcache/JavaSpringCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dunkelhai.javaspringcache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringCacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
